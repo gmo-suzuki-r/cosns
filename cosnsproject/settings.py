0cc4554c0367f41258d87713df4cff304fdd4505
Suzuki Ren <suzuki-r@gmo-connect.jp>
2023-01-06 15:57:44 +0900

    no message

--- cosnsproject/settings.py
+++ cosnsproject/settings.py
@@ -116,13 +116,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR / "media"
+STATIC_URL = '/sta/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
 MEDIA_URL = "medi/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_URL = 'login'
